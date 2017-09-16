4c4e75b8ead0821fe1258be717439d6a0988f7e3
Cédric Clément <cclem054@uottawa.ca>
2017-09-15 21:56:48 -0400

    added more methods for writing to files.

--- SimpleIO.java
+++ SimpleIO.java
@@ -6,29 +6,10 @@ import java.io.*;
 import java.util.List;
 
 public class SimpleIO{
-    private static final String LAB_01_PATH = "/Users/ced/Documents/uottawa/csi/4139_17/labs/01_encryption/";
+    public static final String LAB_01_PATH = "/Users/ced/Documents/uottawa/csi/4139_17/labs/01_encryption/keys/";
     
     public static void main(String[] args) throws IOException{
-        String b64Apple = SimpleIO.toBase64("Apple");
-        System.out.println(b64Apple);
-        String apple = SimpleIO.fromBase64(b64Apple);
-        System.out.println(apple);
-        
-        String fileContents = SimpleIO.readFile(LAB_01_PATH + "file");
-        System.out.println(fileContents);
-        
-        SimpleIO.writeFile(LAB_01_PATH + "testFile", "content for test file");
-        SimpleIO.writeFile(LAB_01_PATH + "testFile", "more content");
-        String otherContents = SimpleIO.readFile(LAB_01_PATH + "testFile");
-        System.out.println(otherContents);
-        
-        String message = SimpleIO.toBase64("Hello world, this is a base64 encoded message");
-        SimpleIO.writeFile(LAB_01_PATH + "base64", message);
-        String fileRead = SimpleIO.readFile(LAB_01_PATH + "base64");
-        System.out.println("Encoded message read from encoded file: " + fileRead);
 
-        String decodedMessage = SimpleIO.fromBase64(fileRead);
-        System.out.println("Decoded message read from encoded file: " + decodedMessage);
     }
 
     /**
@@ -56,7 +37,7 @@ public class SimpleIO{
      * @param string path The path of the file to read the content from.
      * @return string The content read from the file.
      */
-    public static String readFile(String path) throws IOException, FileNotFoundException{
+    public static String readStringFromFile(String path) throws IOException, FileNotFoundException{
         List<String> content = Files.readAllLines(Paths.get(path));
         String ret           = String.join("\n", content);
         return ret;
@@ -68,11 +49,69 @@ public class SimpleIO{
      * @param string content The content to write to the file.
      * @return void
      */
-    public static void writeFile(String path, String content) throws IOException{
+    public static void writeStringToFile(String path, String content) throws IOException{
         FileOutputStream fos = new FileOutputStream(path);
         fos.write(content.getBytes());
         fos.close();
     }
     
+    
+    /**
+     * Writes content to a file
+     * @param String path
+     * @param byte[] content
+     * @return void
+     */
+    public static void writeBytesToFile(String path, byte[] content) throws IOException{
+        FileOutputStream fos = new FileOutputStream(path);
+        fos.write(content);
+        fos.close();
+    }
+
+    /**
+     * Gets the content of a file.
+     * @param string path The path of the file to read the content from.
+     * @return byte[] The content read from the file.
+     */
+    public static byte[] readBytesFromFile(String path) throws IOException {
+        byte[] content = Files.readAllBytes(Paths.get(path));
+        return content;
+    }    
+
+
+
+
+
+
+    public static void writeBytes(File output, byte[] toWrite) throws IOException{
+        output.getParentFile().mkdirs();
+        FileOutputStream fos = new FileOutputStream(output);
+        fos.write(toWrite);
+        fos.flush();
+        fos.close();
+    }
+    
+    public static byte[] readBytes(File f) throws IOException{
+        FileInputStream fis = new FileInputStream(f);
+        byte[] fbytes = new byte[(int) f.length()];
+        fis.read(fbytes);
+        fis.close();
+        return fbytes;
+    }
+    
+    public static void writeContent(String path, String content) throws IOException{
+        File file = new File(path);
+        SimpleIO.writeBytes(file, content.getBytes());
+    }
+    
+    public static String readContent(String path) throws IOException{
+        File file = new File(path);
+        byte[] contents = SimpleIO.readBytes(file);
+        return new String(contents);
+    }
+
+
+
+
     
 }
